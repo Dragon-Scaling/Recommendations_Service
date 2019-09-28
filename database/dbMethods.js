f0b551b8477e18108da5dc78a0f222910abf10db
EC2 Default User <ec2-user@ip-172-31-5-23.us-west-1.compute.internal>
2019-09-28 05:49:22 +0000

    Add exit on error

--- database/dbMethods.js
+++ database/dbMethods.js
@@ -40,6 +40,8 @@ exports.findListing = (req, res, callback) => {
 
   pool.query(input, (error, results) => {
     if (error) {
+      console.log(error);
+      process.exit()
       callback(error);
     } else {
       // console.log('results:', results.rows[0].nearby);
@@ -47,6 +49,8 @@ exports.findListing = (req, res, callback) => {
       const joinQuery = `${generateQuery(array)}`;
       pool.query(joinQuery, (err, data) => {
         if (err) {
+	  console.log(error);
+	  process.exit()
           callback(err);
         } else {
           const processedData = data.rows.map(property => {
